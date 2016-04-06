a3d2c458e37cfde3cbfd7c56d7c1c950a610774e
patrickg112 <patrickg112@gmail.com>
2016-04-06 00:50:32 -0400

    charateristics puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Tail length: varies
+#Friendly: varies
+#Ears count: 2
+#Name: varies
+#Color: varies
+#Siz: varies
+#Ears length: varies
+#Legs count: 4
+
+#BEHAVIOR -----------------------
+#Jump
+#Bark
+#Run
+#Pant
